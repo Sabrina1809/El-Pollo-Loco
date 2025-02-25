411a22341028b685f5d48073c10d40337f72d8cf
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-02-25 16:20:23 +0100

    small chicken is jumping while walking

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -4,7 +4,8 @@ class ChickenSmall extends Chicken {
     width = 60;
     y = 370;
     speed = (Math.random()/2);
-    speedY = 10;
+    speedY = 1.5;
+    
 
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
@@ -18,10 +19,39 @@ class ChickenSmall extends Chicken {
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png')
-        this.x = 800 + Math.random() * 500;
+        this.x = 300 + Math.random() * 500;
         this.loadImages(this.IMAGES_WALKING);
         this.animate();
+        this.acceleration = 0.8;
+        // this.fallingChicken();
+        let jumpingChicken = this.fallingChicken()
+           
+        
+        
+      
         
     }
+
+    fallingChicken() {
+       
+        setTimeout(() => {
+            this.y = 350;
+            // this.y = 370;
+            // this.y = 50;
+            // this.loadImages(this.IMAGES_WALKING);
+            let fallingChicken =  setInterval(() => {
+                if (this.y < 370) {
+                    this.y -= this.speedY;
+                    this.speedY -= this.acceleration;
+                    console.log('fallingChicken erreicht');
+                    
+                }
+              
+            }, 1000/25)
+           
+        
+        }, 2000)
+    }
+
 
 }
