001b851264e00cb4cd0f308397d0eb1ea2529158
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-03-03 08:33:54 +0100

    fullscreen works

--- js/game.js
+++ js/game.js
@@ -9,22 +9,22 @@ function init() {
     
 }
 
-document.getElementById('button-openmenu').addEventListener('click', openMenu)
+document.getElementById('button-openinfo').addEventListener('click', openMenu)
 
 function openMenu() {
     console.log('openMenu erreicht');
-    if (document.getElementById('button-openmenu').classList.contains('closed')) {
-        document.getElementById('button-openmenu').classList.remove('closed');
+    if (document.getElementById('button-openinfo').classList.contains('closed')) {
+        document.getElementById('button-openinfo').classList.remove('closed');
         document.getElementById('info-line').style.width = 'fit-content';
-        document.getElementById('more-buttons').style.width = 'fit-content';
+        // document.getElementById('more-buttons').style.width = 'fit-content';
         document.getElementById('info-line').style.visibility = 'visible';
-        document.getElementById('more-buttons').style.visibility = 'visible';
+        // document.getElementById('more-buttons').style.visibility = 'visible';
     } else {
-        document.getElementById('button-openmenu').classList.add('closed');
+        document.getElementById('button-openinfo').classList.add('closed');
         document.getElementById('info-line').style.width = '0px';
-        document.getElementById('more-buttons').style.width = '0px';
+        // document.getElementById('more-buttons').style.width = '0px';
         document.getElementById('info-line').style.visibility = 'hidden';
-        document.getElementById('more-buttons').style.visibility = 'hidden';
+        // document.getElementById('more-buttons').style.visibility = 'hidden';
     }
 }
 
@@ -124,23 +124,44 @@ document.getElementById('button-right').addEventListener('touchcancel', () => {
     keyboard.RIGHT = false;
 });
 
-document.getElementById('button-home').addEventListener('touchstart', () => {
+document.getElementById('button-home').addEventListener('touchend', () => {
     console.log('home');
 });
-document.getElementById('button-home').addEventListener('touchend', () => {
-    console.log('home end');
-});
 
-document.getElementById('button-screen').addEventListener('touchstart', () => {
+document.getElementById('button-screen').addEventListener('touchend', () => {
     console.log('screen');
 });
-document.getElementById('button-screen').addEventListener('touchend', () => {
-    console.log('screen end');
-});
 
-document.getElementById('button-sound').addEventListener('touchstart', () => {
+
+document.getElementById('button-sound').addEventListener('touchend', () => {
     console.log('sound');
 });
-document.getElementById('button-sound').addEventListener('touchend', () => {
-    console.log('sound end');
-});
+
+document.getElementById("button-screen").addEventListener("click", toggleFullscreen);
+document.getElementById("button-screen").addEventListener("touchstart", toggleFullscreen);
+
+function toggleFullscreen() {
+    let fullscreenElement = document.getElementById("fullscreen");
+
+    if (!document.fullscreenElement) {
+        if (fullscreenElement.requestFullscreen) {
+            fullscreenElement.requestFullscreen();
+        } else if (fullscreenElement.mozRequestFullScreen) { // Firefox
+            fullscreenElement.mozRequestFullScreen();
+        } else if (fullscreenElement.webkitRequestFullscreen) { // Chrome, Safari, Edge
+            fullscreenElement.webkitRequestFullscreen();
+        } else if (fullscreenElement.msRequestFullscreen) { // IE/Edge
+            fullscreenElement.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
